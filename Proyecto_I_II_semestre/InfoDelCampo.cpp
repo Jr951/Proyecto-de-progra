6b880ecb81e319b90d5c3010d8a8b3d18e7a4033
Ivan Zamora <ivanzamora262@gmail.com>
2022-10-07 15:10:22 -0600

    hola

--- Proyecto_I_II_semestre/InfoDelCampo.cpp
+++ Proyecto_I_II_semestre/InfoDelCampo.cpp
@@ -1,22 +1,11 @@
 #include "InfoDelCampo.h"
 
-InfoDelCampo::InfoDelCampo() {
+InfoDelCampo::InfoDelCampo()
+{
 
 }
 
-InfoDelCampo::InfoDelCampo(int *value) {
-
-}
-
-void InfoDelCampo::setNCampo(int *value) {
-
-}
-
-int *InfoDelCampo::getNCampo() {
-
-    return nullptr;
-}
-
-InfoDelCampo::~InfoDelCampo() {
+InfoDelCampo::~InfoDelCampo()
+{
 
 }
--- Proyecto_I_II_semestre/InfoDelCampo.h
+++ Proyecto_I_II_semestre/InfoDelCampo.h
@@ -1,5 +1,16 @@
+#ifndef INFODELCAMPO_H
+#define INFODELCAMPO_H
+
 #pragma once
+
 class InfoDelCampo
 {
+public:
+    InfoDelCampo();
+    ~InfoDelCampo();
+
+private:
+
 };
 
+#endif
