5c2396d61874646a9505b4aa392db85ce9759969
Junior951 <111672444+Jr951@users.noreply.github.com>
2022-10-06 22:37:02 -0600

    Update InfoDelCampo.h

--- Proyecto_I_II_semestre/InfoDelCampo.h
+++ Proyecto_I_II_semestre/InfoDelCampo.h
@@ -1,5 +1,25 @@
 #pragma once
 class InfoDelCampo
 {
+  private:
+	int numero;
+	char estado;
+	Vehiculo** veh;
+	int tam;
+	int cant;
+
+public:
+	void setNumero(int numero);
+	void setestado(char estado);
+	void setTam(int tam);
+	void setcant(int cant);
+
+	int getNmero();
+	char getEstado();
+	int getTam();
+	int getCant();
+
+	Vehiculo** getVehiculo();
+	void setVehiculo(Vehiculo** veh);
 };
 
