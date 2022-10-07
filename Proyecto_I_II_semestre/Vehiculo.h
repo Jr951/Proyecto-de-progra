ffcd7125c719116332095ea17c8b28f67507285f
Junior951 <111672444+Jr951@users.noreply.github.com>
2022-10-06 22:35:30 -0600

    Update Vehiculo.h

--- Proyecto_I_II_semestre/Vehiculo.h
+++ Proyecto_I_II_semestre/Vehiculo.h
@@ -1,5 +1,32 @@
 #pragma once
 class Vehiculo
-{
+{private:
+	string marca;
+	string placa;
+	float tonelaje;
+	string color;
+	Chofer* chofer;
+	Vehiculo* vehiculo;
+
+
+public:
+	Vehiculo(string marca = "", string placa = "", float tonelaje = 0, string color = "", Chofer* chofer = new Chofer(), Vehiculo* vehiculo = new Vehiculo());
+	~Vehiculo();
+
+	void setMarca(string marca);
+	void setPlaca(string placa);
+	void setTonelaje(float tonelaje);
+	void setColor(string color);
+
+	string getMarca();
+	string getPlaca();
+	float getTonelaje();
+	string getColor();
+	
+    Chofer* getChofer() ;
+    void setChofer(Chofer* chofer);
+
+	Vehiculo* getVehiculo() ;
+	void setVehiculo(Vehiculo* vehiculo);
 };
 
