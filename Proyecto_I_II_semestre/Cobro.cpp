dbede01bd19821f59a0becb500e1ccbf9f31ffcc
Ivan Zamora <ivanzamora262@gmail.com>
2022-10-08 00:22:19 -0600

    Modificacion de cobro

--- Proyecto_I_II_semestre/Cobro.cpp
+++ Proyecto_I_II_semestre/Cobro.cpp
@@ -1,1 +1,22 @@
 #include "Cobro.h"
+
+Cobro::Cobro() {}
+
+Cobro::Cobro(int horaP, int minutoP,double montoP) {}
+
+Cobro::~Cobro() {}
+
+void Cobro::setHora(int horaP) {hora=horaP;}
+
+void Cobro::setMinutos(int minutosP) {minuto=minutosP;}
+
+void Cobro::setMonto(double montoP) {monto=montoP;}
+
+int Cobro::getHora() {return hora;}
+
+int Cobro::getMinutos() {return minuto;}
+
+double Cobro::getMonto() {return monto;}
+
+
+
--- Proyecto_I_II_semestre/Cobro.h
+++ Proyecto_I_II_semestre/Cobro.h
@@ -1,5 +1,25 @@
 #pragma once
 class Cobro
 {
+private:
+    int hora=0;
+    int minuto=0;
+    double monto=0;
+public:
+    Cobro();
+    Cobro(int hora,int minuto,double monto);
+    ~Cobro();
+
+    void setHora(int Hora);
+    void setMinutos(int Minutos);
+    void setMonto(double Monto);
+
+    int getHora();
+    int getMinutos();
+    double getMonto();
+
+
+
+
 };
 
